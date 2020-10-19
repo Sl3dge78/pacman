5c406ba84352b25a692a2d05bc3fee4259d639a7
Sl3dge78 <45926471+Sl3dge78@users.noreply.github.com>
2020-10-19 17:55:12 +0200

    Quick refactoring

--- src/game.c
+++ src/game.c
@@ -19,7 +19,17 @@
 #define PTS_FOR_NEW_LIFE 100000
 #define STARTING_LIVES 2
 
-enum {
+/*
+ * DECLARATIONS
+ */
+
+enum Alignement {
+	ALIGN_LEFT = 0,
+	ALIGN_CENTERED,
+	ALIGN_RIGHT
+} typedef Alignement;
+
+enum Tile {
 	PAC = -2,
 	POWERUP = -3,
 	EMPTY = -1,
@@ -31,7 +41,7 @@ enum {
 	STRAIGHT_VER = 5,
 } typedef Tile;
 
-enum {
+enum Direction {
 	EAST = 0,
 	SOUTH = 1,
 	NORTH = 2,
@@ -52,14 +62,14 @@ typedef struct Player {
 	SDL_Texture *texture;
 	Direction direction;
 
-} Player;
-
-typedef struct Scores {
 	int lives;
 	int score;
 	int new_life_pts;
 	int pac_left;
-} Scores;
+
+	bool is_powered_up;
+
+} Player;
 
 typedef struct WaitStateData {
 	int timer;
@@ -103,13 +113,330 @@ typedef struct Game {
 
 	bool is_running;
 
-	Scores *scores;
-
 	SDL_Point camera_position;
 
 } Game;
 
-// Load all resources
+static void switch_state(Game *game, State new_state);
+static void init_game(Player *player);
+
+/*
+ * MAP
+ */
+
+static int get_tile_id(const int x, const int y, const Map *map) {
+	return x + y * map->rect.w;
+}
+
+static Tile get_tile_at_pos(const int x, const int y, const Map *map) {
+	if (x < 0 || x >= map->rect.w || y < 0 || y >= map->rect.h)
+		return EMPTY;
+
+	return map->map[get_tile_id(x, y, map)];
+}
+
+static void toggle_map_color(SDL_Texture *texture) {
+	Uint8 r = 0;
+	SDL_GetTextureColorMod(texture, &r, NULL, NULL);
+
+	if (r == 255)
+		SDL_SetTextureColorMod(texture, 0, 0, 255);
+	else
+		SDL_SetTextureColorMod(texture, 255, 255, 255);
+}
+
+/*
+ * PLAYER
+ */
+static void take_damage(Game *game) {
+	if (game->player->lives-- <= 0) {
+		switch_state(game, STATE_GAMEOVER);
+		return;
+	}
+	switch_state(game, STATE_WAIT);
+}
+
+static void handle_player_movement(int delta_time, Player *player, Map *map, float min_x, float max_x) {
+	float speed = 5.0f * delta_time / 1000.0f;
+	SDL_FPoint new_pos = player->pos;
+	switch (player->direction) {
+		case NORTH: {
+			if (get_tile_at_pos((int)player->pos.x, (int)(player->pos.y - speed - .5f), map) < 0)
+				new_pos.y -= speed;
+		} break;
+		case SOUTH: {
+			if (get_tile_at_pos((int)player->pos.x, (int)(player->pos.y + speed + .5f), map) < 0)
+				new_pos.y += speed;
+		} break;
+		case EAST: {
+			if (get_tile_at_pos((int)(player->pos.x + speed + 0.5f), (int)player->pos.y, map) < 0)
+				new_pos.x += speed;
+		} break;
+		case WEST: {
+			if (get_tile_at_pos((int)(player->pos.x - speed - 0.5f), (int)player->pos.y, map) < 0)
+				new_pos.x -= speed;
+		} break;
+	}
+	player->pos = new_pos;
+
+	// Wrap around
+	if (player->pos.x < min_x) {
+		player->pos.x = max_x;
+	}
+	if (player->pos.x > max_x) {
+		player->pos.x = min_x;
+	}
+}
+
+/*
+ *  UPDATE
+ */
+
+static void switch_state(Game *game, State new_state) {
+	game->state.state = new_state;
+	switch (new_state) {
+		case STATE_START_LEVEL: {
+			SDL_Log("State changed to START");
+			init_game(game->player);
+			switch_state(game, STATE_WAIT);
+		} break;
+
+		case STATE_WAIT: {
+			SDL_Log("State changed to WAIT");
+			game->player->direction = WEST;
+			game->player->pos.x = 14.0f;
+			game->player->pos.y = 23.5f;
+			game->state.wait_state_data.timer = 5000;
+			SDL_SetTextureColorMod(game->map->texture, 0, 0, 255);
+		} break;
+
+		case STATE_NORMAL: {
+			SDL_Log("State changed to NORMAL");
+			game->state.normal_state_data.blink_timer = 0;
+			game->state.normal_state_data.power_up_timer = 0;
+		} break;
+
+		case STATE_WIN: {
+			SDL_Log("WIN!");
+			game->is_running = false;
+		} break;
+
+		case STATE_GAMEOVER: {
+			SDL_Log("GAMEOVER!");
+			game->is_running = false;
+		}
+	}
+}
+
+static void input(SDL_Event *e, Game *game, Player *player) {
+	if (e->type == SDL_KEYDOWN) {
+		switch (e->key.keysym.scancode) {
+			case SDL_SCANCODE_W:
+				player->direction = NORTH;
+				break;
+			case SDL_SCANCODE_S:
+				player->direction = SOUTH;
+				break;
+			case SDL_SCANCODE_A:
+				player->direction = WEST;
+				break;
+			case SDL_SCANCODE_D:
+				player->direction = EAST;
+				break;
+			case SDL_SCANCODE_K:
+				take_damage(game);
+				break;
+		}
+	}
+}
+
+static void update(const int delta_time, Game *game) {
+	switch (game->state.state) {
+		case STATE_WAIT: {
+			WaitStateData *data = &game->state.wait_state_data;
+
+			if (data->timer > 0) {
+				data->timer -= delta_time;
+			} else {
+				switch_state(game, STATE_NORMAL);
+			}
+
+		} break;
+		case STATE_NORMAL: {
+			Player *player = game->player;
+			handle_player_movement(delta_time, player, game->map, game->camera_position.x, game->camera_position.x + game->map->rect.w);
+			int id = get_tile_id(player->pos.x, player->pos.y, game->map);
+			switch (get_tile_at_pos(player->pos.x, player->pos.y, game->map)) {
+				case PAC:
+					game->map->map[id] = EMPTY;
+					player->score += 100;
+					player->new_life_pts -= 100;
+					player->pac_left--;
+					break;
+				case POWERUP:
+					player->is_powered_up = true;
+					game->map->map[id] = EMPTY;
+					game->state.normal_state_data.power_up_timer = POWERUP_MAX_TIME;
+					game->state.normal_state_data.blink_timer = 200;
+					break;
+			}
+			if (player->pac_left <= 0) {
+				switch_state(game, STATE_WIN);
+			}
+			if (player->new_life_pts <= 0) {
+				player->lives++;
+				player->new_life_pts += 100000;
+			}
+			if (player->is_powered_up) {
+				NormalStateData *data = &game->state.normal_state_data;
+				if (data->power_up_timer > 0) {
+					data->power_up_timer -= delta_time;
+					data->blink_timer -= delta_time;
+
+					if (data->blink_timer < 0) {
+						toggle_map_color(game->map->texture);
+						if (data->power_up_timer > 2000)
+							data->blink_timer = 200;
+						else
+							data->blink_timer = 100;
+					}
+
+					if (data->power_up_timer < 0) {
+						data->power_up_timer = 0;
+						SDL_SetTextureColorMod(game->map->texture, 0, 0, 255);
+						player->is_powered_up = false;
+					}
+				}
+			}
+		} break;
+
+		default:
+			break;
+	}
+}
+
+/*
+ *  DRAWING
+ */
+
+static int draw_text(SDL_Renderer *renderer, TTF_Font *font, char *text, const SDL_Point *src, Alignement align) {
+	SDL_Color color = { 255, 255, 255, 255 };
+	SDL_Surface *surf = TTF_RenderText_Solid(font, text, color);
+
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
+	SDL_FreeSurface(surf);
+	int w, h;
+	SDL_QueryTexture(texture, NULL, NULL, &w, &h);
+
+	SDL_Rect dst = { src->x, src->y, w, h };
+
+	switch (align) {
+		case ALIGN_CENTERED:
+			dst.x -= w / 2;
+			dst.y -= h / 2;
+			break;
+		case ALIGN_LEFT:
+			break;
+		case ALIGN_RIGHT:
+			dst.x -= w;
+			break;
+	}
+
+	SDL_RenderCopy(renderer, texture, NULL, &dst);
+	SDL_DestroyTexture(texture);
+
+	return w + src->x;
+}
+
+static void draw_ui(SDL_Renderer *renderer, TTF_Font *font, Player *player, State current_state) {
+	SDL_Point place = { 0, 0 };
+
+	// Score
+	char score_str[16];
+	sprintf_s(score_str, 16 * sizeof(char), "Score : %06d", player->score);
+	place.x = draw_text(renderer, font, score_str, &place, ALIGN_LEFT);
+
+	place.x += 16;
+
+	char new_life_str[16];
+	sprintf_s(new_life_str, 16 * sizeof(char), "1UP : %06d", player->new_life_pts);
+	place.x = draw_text(renderer, font, new_life_str, &place, ALIGN_LEFT);
+
+	place.x += 16;
+
+	// Lives
+	for (int i = 0; i < player->lives; i++) {
+		SDL_Rect src = { 0, 0, 16, 16 };
+		SDL_Rect dst = { place.x, 0, 16, 16 };
+		place.x += 16;
+		SDL_RenderCopy(renderer, player->texture, &src, &dst);
+	}
+
+	switch (current_state) {
+		case STATE_WAIT: {
+			SDL_Point center = { MAP_WIDTH * 16 / 2, MAP_HEIGHT * 16 / 2 + 48 };
+			place.x = draw_text(renderer, font, "GET READY!", &center, ALIGN_CENTERED);
+		} break;
+	}
+}
+
+static void draw_player(SDL_Renderer *renderer, Player *player, SDL_Point *camera_offset) {
+	SDL_Rect src = { player->direction % 2 * 16, player->direction / 2 * 16, 16, 16 };
+	SDL_Rect dst = { (int)(player->pos.x * 16.0f) - 8 + camera_offset->x, (int)(player->pos.y * 16.0f) - 8 + camera_offset->y, 16, 16 };
+
+	SDL_RenderCopy(renderer, player->texture, &src, &dst);
+}
+
+static void draw_map(SDL_Renderer *renderer, Map *map, SDL_Point *camera_offset) {
+	SDL_Rect src = { 0, 0, 16, 16 };
+	SDL_Rect dst = { 0, 0, 16, 16 };
+
+	for (Uint32 y = 0; y < map->rect.h; y++) {
+		for (Uint32 x = 0; x < map->rect.w; x++) {
+			Uint32 val = x + y * map->rect.h;
+
+			switch (map->map[x + y * map->rect.w]) {
+				case EMPTY: {
+					continue;
+				} break;
+				case PAC: {
+					SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+					SDL_Rect pac = { x * 16 + 6 + camera_offset->x, y * 16 + 6 + camera_offset->y, 4, 4 };
+					SDL_RenderDrawRect(renderer, &pac);
+				} break;
+				case POWERUP: {
+					SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+					SDL_Rect pup = { x * 16 + 2 + camera_offset->x, y * 16 + 2 + camera_offset->y, 14, 14 };
+					SDL_RenderDrawRect(renderer, &pup);
+				} break;
+				default: {
+					src.x = map->map[x + y * map->rect.w] % 3 * 16;
+					src.y = map->map[x + y * map->rect.w] / 3 * 16;
+					dst.x = x * 16 + camera_offset->x;
+					dst.y = y * 16 + camera_offset->y;
+					SDL_RenderCopy(renderer, map->texture, &src, &dst);
+				} break;
+			}
+		}
+	}
+}
+
+static void draw(SDL_Renderer *renderer, Game *game) {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_RenderClear(renderer);
+
+	draw_map(renderer, game->map, &game->camera_position);
+	draw_player(renderer, game->player, &game->camera_position);
+
+	draw_ui(renderer, game->font, game->player, game->state.state);
+
+	SDL_RenderPresent(renderer);
+}
+
+/* 
+ * CORE
+ */
+
 static Game *load_resources(SDL_Renderer *renderer) {
 	Game *game = malloc(sizeof(Game));
 
@@ -164,324 +491,20 @@ static Game *load_resources(SDL_Renderer *renderer) {
 	game->map->rect.w = MAP_WIDTH;
 	game->map->rect.h = MAP_HEIGHT;
 
-	Scores *scores = calloc(1, sizeof(Scores));
-	game->scores = scores;
-
 	game->camera_position.x = 0;
 	game->camera_position.y = 16;
 
 	return game;
 }
 
-// Reset game state to starting
-static void init_game(Scores *scores) {
-	scores->score = 0;
-	scores->pac_left = PAC_AMOUNT;
-	scores->new_life_pts = PTS_FOR_NEW_LIFE;
-	scores->lives = STARTING_LIVES;
-	scores->new_life_pts = PTS_FOR_NEW_LIFE;
+static void init_game(Player *player) {
+	player->score = 0;
+	player->pac_left = PAC_AMOUNT;
+	player->new_life_pts = PTS_FOR_NEW_LIFE;
+	player->lives = STARTING_LIVES;
+	player->new_life_pts = PTS_FOR_NEW_LIFE;
+	player->is_powered_up = false;
 }
-
-#pragma region UPDATE
-/*************
-    UPDATE
-**************/
-
-static void switch_state(Game *game, State new_state) {
-	game->state.state = new_state;
-	switch (new_state) {
-		case STATE_START_LEVEL: {
-			SDL_Log("State changed to START");
-			init_game(game->scores);
-			switch_state(game, STATE_WAIT);
-		} break;
-
-		case STATE_WAIT: {
-			SDL_Log("State changed to WAIT");
-			game->player->direction = WEST;
-			game->player->pos.x = 14.0f;
-			game->player->pos.y = 23.5f;
-			game->state.wait_state_data.timer = 5000;
-			SDL_SetTextureColorMod(game->map->texture, 0, 0, 255);
-		} break;
-
-		case STATE_NORMAL: {
-			SDL_Log("State changed to NORMAL");
-			game->state.normal_state_data.blink_timer = 0;
-			game->state.normal_state_data.power_up_timer = 0;
-		} break;
-
-		case STATE_WIN: {
-			SDL_Log("WIN!");
-			game->is_running = false;
-		} break;
-
-		case STATE_GAMEOVER: {
-			SDL_Log("GAMEOVER!");
-			game->is_running = false;
-		}
-	}
-}
-
-static Tile get_tile_at_pos(const int x, const int y, const Map *map) {
-	if (x < 0 || x >= map->rect.w || y < 0 || y >= map->rect.h)
-		return EMPTY;
-
-	return map->map[x + y * map->rect.w];
-}
-
-static void toggle_map_color(SDL_Texture *texture) {
-	Uint8 r = 0;
-	SDL_GetTextureColorMod(texture, &r, NULL, NULL);
-
-	if (r == 255)
-		SDL_SetTextureColorMod(texture, 0, 0, 255);
-	else
-		SDL_SetTextureColorMod(texture, 255, 255, 255);
-}
-
-static void eat_at_pos(const int x, const int y, Map *map, Scores *scores, NormalStateData *data) {
-	switch (get_tile_at_pos(x, y, map)) {
-		case PAC:
-			map->map[x + y * map->rect.w] = EMPTY;
-			scores->score += 100;
-			scores->new_life_pts -= 100;
-			scores->pac_left--;
-			break;
-		case POWERUP:
-			map->map[x + y * map->rect.w] = EMPTY;
-			data->power_up_timer = POWERUP_MAX_TIME;
-			data->blink_timer = 200;
-			break;
-	}
-}
-
-static void take_damage(Game *game) {
-	if (game->scores->lives-- <= 0) {
-		switch_state(game, STATE_GAMEOVER);
-		return;
-	}
-	switch_state(game, STATE_WAIT);
-}
-
-static void input(SDL_Event *e, Game *game, Player *player) {
-	if (e->type == SDL_KEYDOWN) {
-		switch (e->key.keysym.scancode) {
-			case SDL_SCANCODE_W:
-				player->direction = NORTH;
-				break;
-			case SDL_SCANCODE_S:
-				player->direction = SOUTH;
-				break;
-			case SDL_SCANCODE_A:
-				player->direction = WEST;
-				break;
-			case SDL_SCANCODE_D:
-				player->direction = EAST;
-				break;
-			case SDL_SCANCODE_K:
-				take_damage(game);
-				break;
-		}
-	}
-}
-
-static void handle_player_movement(int delta_time, Player *player, Map *map, float min_x, float max_x) {
-	float speed = 5.0f * delta_time / 1000.0f;
-	SDL_FPoint new_pos = player->pos;
-	switch (player->direction) {
-		case NORTH: {
-			if (get_tile_at_pos((int)player->pos.x, (int)(player->pos.y - speed - .5f), map) < 0)
-				new_pos.y -= speed;
-		} break;
-		case SOUTH: {
-			if (get_tile_at_pos((int)player->pos.x, (int)(player->pos.y + speed + .5f), map) < 0)
-				new_pos.y += speed;
-		} break;
-		case EAST: {
-			if (get_tile_at_pos((int)(player->pos.x + speed + 0.5f), (int)player->pos.y, map) < 0)
-				new_pos.x += speed;
-		} break;
-		case WEST: {
-			if (get_tile_at_pos((int)(player->pos.x - speed - 0.5f), (int)player->pos.y, map) < 0)
-				new_pos.x -= speed;
-		} break;
-	}
-	player->pos = new_pos;
-
-	// Wrap around
-	if (player->pos.x < min_x) {
-		player->pos.x = max_x;
-	}
-	if (player->pos.x > max_x) {
-		player->pos.x = min_x;
-	}
-}
-
-static void update(const int delta_time, Game *game) {
-	switch (game->state.state) {
-		case STATE_WAIT: {
-			WaitStateData *data = &game->state.wait_state_data;
-
-			if (data->timer > 0) {
-				data->timer -= delta_time;
-			} else {
-				switch_state(game, STATE_NORMAL);
-			}
-
-		} break;
-		case STATE_NORMAL: {
-			handle_player_movement(delta_time, game->player, game->map, game->camera_position.x, game->camera_position.x + game->map->rect.w);
-			eat_at_pos((int)game->player->pos.x, (int)game->player->pos.y, game->map, game->scores, &game->state.normal_state_data);
-			if (game->scores->new_life_pts <= 0) {
-				game->scores->lives++;
-				game->scores->new_life_pts += 10000;
-			}
-
-			if (game->scores->pac_left <= 0) {
-				switch_state(game, STATE_WIN);
-			}
-
-			NormalStateData *data = &game->state.normal_state_data;
-			if (data->power_up_timer > 0) {
-				data->power_up_timer -= delta_time;
-				data->blink_timer -= delta_time;
-
-				if (data->power_up_timer < 0) {
-					data->power_up_timer = 0;
-					SDL_SetTextureColorMod(game->map->texture, 0, 0, 255);
-				}
-
-				if (data->blink_timer < 0) {
-					toggle_map_color(game->map->texture);
-					if (data->power_up_timer > 2000)
-						data->blink_timer = 200;
-					else
-						data->blink_timer = 100;
-				}
-			}
-		} break;
-
-		default:
-			break;
-	}
-}
-
-#pragma endregion
-
-#pragma region DRAWING
-/**************
-    DRAWING
-***************/
-
-static int draw_text(SDL_Renderer *renderer, TTF_Font *font, char *text, const SDL_Point *src) {
-	SDL_Color color = { 255, 255, 255, 255 };
-	SDL_Surface *surf = TTF_RenderText_Solid(font, text, color);
-
-	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
-	SDL_FreeSurface(surf);
-	int w, h;
-	SDL_QueryTexture(texture, NULL, NULL, &w, &h);
-
-	SDL_Rect dst = { src->x, src->y, w, h };
-
-	SDL_RenderCopy(renderer, texture, NULL, &dst);
-	SDL_DestroyTexture(texture);
-
-	return w + src->x;
-}
-
-static void draw_ui(SDL_Renderer *renderer, TTF_Font *font, Scores *scores, SDL_Texture *player_texture, State current_state) {
-	SDL_Point place = { 0, 0 };
-
-	// Score
-	char score_str[16];
-	sprintf_s(score_str, 16 * sizeof(char), "Score : %06d", scores->score);
-	place.x = draw_text(renderer, font, score_str, &place);
-
-	place.x += 16;
-
-	char new_life_str[16];
-	sprintf_s(new_life_str, 16 * sizeof(char), "1UP : %06d", scores->new_life_pts);
-	place.x = draw_text(renderer, font, new_life_str, &place);
-
-	place.x += 16;
-
-	// Lives
-	for (int i = 0; i < scores->lives; i++) {
-		SDL_Rect src = { 0, 0, 16, 16 };
-		SDL_Rect dst = { place.x, 0, 16, 16 };
-		place.x += 16;
-		SDL_RenderCopy(renderer, player_texture, &src, &dst);
-	}
-
-	switch (current_state) {
-		case STATE_WAIT: {
-			place.x = draw_text(renderer, font, "WAIT", &place);
-		} break;
-		case STATE_NORMAL: {
-			place.x = draw_text(renderer, font, "NORMAL", &place);
-		} break;
-		case STATE_WIN: {
-			place.x = draw_text(renderer, font, "WIN", &place);
-		} break;
-	}
-}
-
-static void draw_player(SDL_Renderer *renderer, Player *player, SDL_Point *camera_offset) {
-	SDL_Rect src = { player->direction % 2 * 16, player->direction / 2 * 16, 16, 16 };
-	SDL_Rect dst = { (int)(player->pos.x * 16.0f) - 8 + camera_offset->x, (int)(player->pos.y * 16.0f) - 8 + camera_offset->y, 16, 16 };
-
-	SDL_RenderCopy(renderer, player->texture, &src, &dst);
-}
-
-static void draw_map(SDL_Renderer *renderer, Map *map, SDL_Point *camera_offset) {
-	SDL_Rect src = { 0, 0, 16, 16 };
-	SDL_Rect dst = { 0, 0, 16, 16 };
-
-	for (Uint32 y = 0; y < map->rect.h; y++) {
-		for (Uint32 x = 0; x < map->rect.w; x++) {
-			Uint32 val = x + y * map->rect.h;
-
-			switch (map->map[x + y * map->rect.w]) {
-				case EMPTY: {
-					continue;
-				} break;
-				case PAC: {
-					SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-					SDL_Rect pac = { x * 16 + 6 + camera_offset->x, y * 16 + 6 + camera_offset->y, 4, 4 };
-					SDL_RenderDrawRect(renderer, &pac);
-				} break;
-				case POWERUP: {
-					SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-					SDL_Rect pup = { x * 16 + 2 + camera_offset->x, y * 16 + 2 + camera_offset->y, 14, 14 };
-					SDL_RenderDrawRect(renderer, &pup);
-				} break;
-				default: {
-					src.x = map->map[x + y * map->rect.w] % 3 * 16;
-					src.y = map->map[x + y * map->rect.w] / 3 * 16;
-					dst.x = x * 16 + camera_offset->x;
-					dst.y = y * 16 + camera_offset->y;
-					SDL_RenderCopy(renderer, map->texture, &src, &dst);
-				} break;
-			}
-		}
-	}
-}
-
-static void draw(SDL_Renderer *renderer, Game *game) {
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-	SDL_RenderClear(renderer);
-
-	draw_map(renderer, game->map, &game->camera_position);
-	draw_player(renderer, game->player, &game->camera_position);
-
-	draw_ui(renderer, game->font, game->scores, game->player->texture, game->state.state);
-
-	SDL_RenderPresent(renderer);
-}
-
-#pragma endregion
 
 static void destroy_game(Game *game) {
 	TTF_CloseFont(game->font);
@@ -491,8 +514,6 @@ static void destroy_game(Game *game) {
 
 	SDL_DestroyTexture(game->map->texture);
 	free(game->map);
-
-	free(game->scores);
 
 	free(game);
 }
